2f50d904f3931df2b19d7745b309365cc063bc12
viniciusrodriguesa <vini.vini.rodriguesa@gmail.com>
2025-07-25 11:40:35 -0300

    Atualização para imprimir os dados corretos

--- index.js
+++ index.js
@@ -4,29 +4,24 @@ const app = express();
 app.get('/', (req, res) => {
   res.json({
     message: 'API online',
-    availableEndpoints: ['/api/stations']
+    availableEndpoints: ['/api/stations'],
+    stations: [
+      {
+        id: '001',
+        name: 'Estação A',
+        measurements: [
+          { date: '2025-07-20', waterLevel: 2.5 },
+          { date: '2025-07-21', waterLevel: 2.7 }
+        ]
+      },
+      {
+        id: '002',
+        name: 'Estação B',
+        measurements: [
+          { date: '2025-07-20', waterLevel: 1.8 },
+          { date: '2025-07-21', waterLevel: 2.0 }
+        ]
+      }
+    ]
   });
 });
-
-app.get('/api/stations', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Estação A',
-      measurements: [
-        { date: '2025-07-20', waterLevel: 2.5 },
-        { date: '2025-07-21', waterLevel: 2.7 }
-      ]
-    },
-    {
-      id: '002',
-      name: 'Estação B',
-      measurements: [
-        { date: '2025-07-20', waterLevel: 1.8 },
-        { date: '2025-07-21', waterLevel: 2.0 }
-      ]
-    }
-  ]);
-});
-
-module.exports = app;
